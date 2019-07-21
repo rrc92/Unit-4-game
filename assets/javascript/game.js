7cf7c8a5ff7056f40a1d53de968d42b28ae3fcf8
Ricardo Cabrera <ricky_92rc@hotmail.com>
2019-07-20 23:52:52 -0700

    editing jquery and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+
+    var c;
+
+    function generaterandomnum(){
+      c = Math.floor(Math.random()*100);
+    }
+
     var win = 0;
     var a = $('#w');
     a.text(win);
@@ -15,29 +22,46 @@ $(document).ready(function(){
     var usec = 0;
     var d = $('#userc');
     d.text(usec);
+
+    function checkNum(n){
+        if (c == n){
+          $('#w').text('you won');
+        }else {
+          $('#l').text('you lost');
+        }
+        $('#compc').text(c);
+      }
     
     function updateusecwithnum(){
 		d.text(usec);
 			
 	}
     $('#rock1').on('click', function(){
-		usec++;
-		updateusecwithnum();
+		// usec++;
+        updateusecwithnum();
+        generaterandomnum();
+      checkNum(1);
     });
     $('#rock2').on('click', function(){
-		usec++;
-		updateusecwithnum();
+		// usec++;
+        updateusecwithnum();
+        generaterandomnum();
+      checkNum(2);
     });
     $('#rock3').on('click', function(){
-		usec++;
-		updateusecwithnum();
+		// usec++;
+        updateusecwithnum();
+        generaterandomnum();
+      checkNum(3);
     });
     $('#rock4').on('click', function(){
-		usec++;
-		updateusecwithnum();
+		// usec++;
+        updateusecwithnum();
+        generaterandomnum();
+      checkNum(4);
     });
     
-    
+   
    
 			
 	
